ce3462100f37bbcf420940f3f15f048a9198a9a4
104000045 <jacky860226@yahoo.com.tw>
2019-08-14 09:49:56 +0800

    add typeOnly flag

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,19 @@ int main(int argc, char *argv[])
 
     std::string jsonPath_1, jsonPath_2;
     std::string contract_1, contract_2;
+    bool compareTypeOnly = false;
 
     app.add_option("--jsonPath_1", jsonPath_1, "Path of first state-variables json folder.")->required();
     app.add_option("--contract_1", contract_1, "The first contract you want to compare.")->required();
 
     app.add_option("--jsonPath_2", jsonPath_2, "Path of second state-variables json folder.")->required();
     app.add_option("--contract_2", contract_2, "The second contract you want to compare.")->required();
+
+    app.add_flag("--typeOnly", compareTypeOnly, "Add this flag will only compare the variable storage size.");
     
     CLI11_PARSE(app, argc, argv);
 
-    Checker checker(jsonPath_1, contract_1, jsonPath_2, contract_2, std::cout, std::cerr, false);
+    Checker checker(jsonPath_1, contract_1, jsonPath_2, contract_2, std::cout, std::cerr, compareTypeOnly);
 
     if(checker.run()) std::cout<<"same\n";
     else std::cout<<"not same\n";
